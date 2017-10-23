175097e17a0aa4e8d1dafc09d07ec82b5d912145
Gu Zheng <cengku@gmail.com>
2017-10-23 23:05:27 +0800

    dcp: add daemon mode support
    Add "-D" option to run dcp as daemon mode, listen the request (flist and only work with flist), add do the cp action with the received flist.
    Server:
    mpirun -np 4 ./install/bin/dcp -D <src> <dest>
    
    Client (only support json format currently):
    curl -H "Content-Type: application/json" -X POST  --data '{"flist":"/tmp/input_list"}' http://127.0.0.1:8888
    
    Request libmicrohttpd and c-json.
    
    Signed-off-by: Gu Zheng <cengku@gmail.com>

--- src/dcp/dcp.c
+++ src/dcp/dcp.c
@@ -27,9 +27,226 @@
 #include <unistd.h>
 #include <errno.h>
 
+#include <sys/types.h>     
+#include <sys/socket.h>     
+#include <netinet/in.h>     
+#include <arpa/inet.h>   
+#include <sys/epoll.h>  
+#include <signal.h>
+#include <syslog.h>
+
 #include "mpi.h"
 #include "libcircle.h"
 #include "mfu.h" 
+
+#define EPOLL_PORT 8000
+
+#define BUFFER_SIZE 256 
+#define MAX_EVENTS 10  
+  
+#include <sys/select.h>
+#include <sys/param.h>   
+#include <sys/types.h>   
+#include <sys/stat.h>
+
+#include <json-c/json.h>
+#include <microhttpd.h>
+
+#define PORT            8888
+#define POSTBUFFERSIZE  32768
+#define MAXNAMESIZE     20
+#define MAXANSWERSIZE   512
+
+#ifndef PATH_MAX
+#define PATH_MAX	256
+#endif
+
+#define GET             0
+#define POST            1
+
+#define KEY_CONTENT_TYPE     "Content-Type"
+#define KEY_CONTENT_JSON     "application/json"
+
+struct MHD_Daemon *g_daemon;
+
+struct connection_info_struct
+{
+	int connectiontype;
+	char *answerstring;
+	struct MHD_PostProcessor *postprocessor;
+};
+
+const char *askpage = "<html><body>\
+                       mpifileutils http daemon<br>\
+                       <form action=\"/namepost\" method=\"post\">\
+                       <input name=\"name\" type=\"text\"\
+                       <input type=\"submit\" value=\" Send \"></form>\
+                       </body></html>";
+
+const char *greetingpage =
+	"<html><body><h1>Welcome, mpifileutils!</center></h1></body></html>";
+
+const char *errorpage =
+       "<html><body><h1>This doesn't seem to be right.</center></h1></body></html>";
+
+#define HTTP_RESPONSE_400_ERRREQ		\
+    "{\"status\":{\"code\":\"400\","		\
+    "\"description\":\"Invalid json format\"}}"
+
+#define HTTP_RESPONSE_404_NOTFOUND		\
+    "{\"status\":{\"code\":\"404\","		\
+    "\"description\":\"Resource Not Found\"}}"
+
+#define HTTP_RESPONSE_201_CREATED		\
+    "{\"status\":{\"code\":\"201\","		\
+    "\"description\":\"Resource Created\"}}"
+
+#define HTTP_RESPONSE_200_OK			\
+    "{\"status\":{\"code\":\"200\","		\
+    "\"description\":\"OK\"}}"
+
+char flist_name[BUFFER_SIZE];
+
+char *json_get_value(const json_object *obj, char *name)
+{
+    if (!obj)
+        return NULL;
+
+    json_object_object_foreach(obj, key, val) {
+        if (!strncmp(name, key, strlen(name))) {
+            json_type type = json_object_get_type(val);
+
+            if (type == json_type_string)
+                return (char *)json_object_get_string(val);
+            MFU_LOG(MFU_LOG_ERR, "Invalid value format, expected is string!\n");
+        }
+    }
+    return NULL;
+}
+
+int get_input_flist(json_object *obj, char *buff)
+{
+    char *value;
+
+    memset(buff, 0, BUFFER_SIZE);
+    value = json_get_value(obj, "flist");
+    if (value && strlen(value)) {
+        strncpy(buff, value, strlen(value));
+        return 0;
+    }
+
+    return -1;
+}
+
+static int check_json_content(void *cls, enum MHD_ValueKind kind, 
+                              const char *key, const char *value)
+{
+    int *has_json = cls;
+
+    if (strncmp(key, KEY_CONTENT_TYPE, strlen(KEY_CONTENT_TYPE)) == 0 &&
+        strncmp(value, KEY_CONTENT_JSON, strlen(KEY_CONTENT_JSON)) == 0)
+        *has_json = 1;
+
+    return MHD_YES;
+}
+
+static int send_page(struct MHD_Connection *connection, const char *page)
+{
+    int ret;
+    struct MHD_Response *response;
+
+    response =
+        MHD_create_response_from_buffer(strlen(page), (void *)page,
+                                        MHD_RESPMEM_PERSISTENT);
+    if (!response)
+        return MHD_NO;
+
+    ret = MHD_queue_response(connection, MHD_HTTP_OK, response);
+    MHD_destroy_response(response);
+
+    return ret;
+}
+
+static void request_completed(void *cls, struct MHD_Connection *connection,
+                              void **con_cls, enum MHD_RequestTerminationCode toe)
+{
+    struct connection_info_struct *con_info = *con_cls;
+
+    if (con_info == NULL)
+        return;
+
+    if (con_info->answerstring)
+        free(con_info->answerstring);
+
+    free(con_info);
+    *con_cls = NULL;
+}
+
+static int answer_to_connection(void *cls, struct MHD_Connection *connection,
+                                const char *url, const char *method,
+                                const char *version, const char *upload_data,
+                                size_t *upload_data_size, void **con_cls)
+{
+    const char * page = cls;
+    int has_json = 0;
+
+    MHD_get_connection_values(connection, MHD_HEADER_KIND,
+                              &check_json_content, &has_json);
+
+    if (*con_cls == NULL) {
+        struct connection_info_struct *con_info;
+
+        con_info = malloc(sizeof(struct connection_info_struct));
+        if (con_info == NULL)
+            return MHD_NO;
+
+        con_info->answerstring = NULL;
+
+        if (!strcmp(method, "POST"))
+            con_info->connectiontype = POST;
+        else
+            con_info->connectiontype = GET;
+
+        *con_cls = (void *)con_info;
+
+        return MHD_YES;
+    }
+
+    if (!strcmp(method, "GET")) {
+        if (has_json) {
+            MFU_LOG(MFU_LOG_ERR, "Invalid form for GET!\n");
+            return send_page(connection, errorpage);
+        }
+
+        return send_page(connection, askpage);
+    }
+
+    if (!strcmp (method, "POST")) {
+        struct connection_info_struct *con_info = *con_cls;
+
+        if (*upload_data_size) {
+            con_info->answerstring = malloc(*upload_data_size + 1);
+            strncpy(con_info->answerstring, upload_data, *upload_data_size);
+            con_info->answerstring[*upload_data_size] = 0;
+            *upload_data_size = 0;
+
+            return MHD_YES;
+        } else if (has_json) {
+            int ret;
+
+            MFU_LOG(MFU_LOG_INFO, "Post data: %s\n", con_info->answerstring);
+            json_object *obj = json_tokener_parse(con_info->answerstring);
+            ret = get_input_flist(obj, flist_name);
+            json_object_put(obj);
+            if (!ret)
+                return send_page(connection, HTTP_RESPONSE_200_OK);
+        }
+        MFU_LOG(MFU_LOG_ERR, "Invalid post format:[%s]\n", con_info->answerstring);
+        return send_page(connection, HTTP_RESPONSE_400_ERRREQ);
+    }
+
+    return send_page(connection, HTTP_RESPONSE_404_NOTFOUND);
+}
 
 /** Print a usage message. */
 void print_usage(void)
@@ -48,9 +265,32 @@ void print_usage(void)
     printf("  -s, --synchronous   - use synchronous read/write calls (O_DIRECT)\n");
     printf("  -S, --sparse        - create sparse files when possible\n");
     printf("  -v, --verbose       - verbose output\n");
+    printf("  -D, --daemon        - run as daemon\n");
     printf("  -h, --help          - print usage\n");
     printf("\n");
     fflush(stdout);
+}
+
+void do_copy_with_flist(const char *inputname, mfu_flist flist, int numpaths_src,
+                        mfu_param_path* paths, const mfu_param_path* destpath,
+                        mfu_copy_opts_t* mfu_copy_opts)
+{
+    struct mfu_flist_skip_args skip_args;
+
+    /* otherwise, read list of files from input, but then stat each one */
+    mfu_flist input_flist = mfu_flist_new();
+    mfu_flist_read_cache(inputname, input_flist);
+
+    skip_args.numpaths = numpaths_src;
+    skip_args.paths = paths;
+    mfu_flist_stat(input_flist, flist, mfu_input_flist_skip, (void *)&skip_args);
+    mfu_flist_free(&input_flist);
+
+    /* copy flist into destination */ 
+    mfu_flist_copy(flist, numpaths_src, paths, destpath, mfu_copy_opts);
+
+    mfu_flist_free(&flist);
+    flist = mfu_flist_new();
 }
 
 int main(int argc, \
@@ -61,8 +301,9 @@ int main(int argc, \
     mfu_init();
 
     /* get our rank */
-    int rank;
+    int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     /* pointer to mfu_copy opts */
     mfu_copy_opts_t mfu_cp_opts; 
@@ -80,9 +321,6 @@ int main(int argc, \
     /* By default, don't have iput file. */
     char* inputname = NULL;
 
-    /* flag to record whether we're walking directories */
-    int walk = 0;
-
     /* By default, don't bother to preserve all attributes. */
     mfu_copy_opts->preserve = 0;
 
@@ -95,6 +333,9 @@ int main(int argc, \
     /* By default, don't use sparse file. */
     mfu_copy_opts->sparse = 0; 
 
+    /* Daemon mode */
+    int run_as_daemon = 0;
+
     int option_index = 0;
     static struct option long_options[] = {
         {"debug"                , required_argument, 0, 'd'},
@@ -104,6 +345,7 @@ int main(int argc, \
         {"synchronous"          , no_argument      , 0, 's'},
         {"sparse"               , no_argument      , 0, 'S'},
         {"verbose"              , no_argument      , 0, 'v'},
+        {"daemon"               , no_argument      , 0, 'D'},
         {"help"                 , no_argument      , 0, 'h'},
         {0                      , 0                , 0, 0  }
     };
@@ -112,7 +354,7 @@ int main(int argc, \
     int usage = 0;
     while(1) {
         int c = getopt_long(
-                    argc, argv, "d:g:hi:pusSv",
+                    argc, argv, "d:g:hi:pusSvD",
                     long_options, &option_index
                 );
 
@@ -200,6 +442,9 @@ int main(int argc, \
             case 'v':
                 mfu_debug_level = MFU_LOG_VERBOSE;
                 break;
+            case 'D':
+                run_as_daemon = 1;
+                break;
             case 'h':
                 usage = 1;
                 break;
@@ -215,11 +460,9 @@ int main(int argc, \
 
     /* paths to walk come after the options */
     int numpaths = 0;
+    int numpaths_src = 0;
     mfu_param_path* paths = NULL;
     if (optind < argc) {
-        /* got a path to walk */
-        walk = 1;
-
         /* determine number of paths specified by user */
         numpaths = argc - optind;
 
@@ -232,22 +475,23 @@ int main(int argc, \
 
         /* advance to next set of options */
         optind += numpaths;
-
-        /* don't allow input file and walk */
-        if (inputname != NULL) {
-            usage = 1;
-        }
-    }
-    else {
-        /* if we're not walking, we must be reading,
-         * and for that we need a file */
-        if (inputname == NULL) {
-            usage = 1;
-        }
+    
+        /* the last path is the destination path, all others are source paths */
+        numpaths_src = numpaths - 1;
     }
 
-    /* the last path is the destination path, all others are source paths */
-    int numpaths_src = numpaths - 1;
+    if (numpaths_src == 0) {
+        if(rank == 0) {
+            MFU_LOG(MFU_LOG_ERR, "No source path found, at least one");
+            print_usage();
+        }
+
+        mfu_param_path_free_all(numpaths, paths);
+        mfu_free(&paths);
+        mfu_finalize();
+        MPI_Finalize();
+        return 1;
+    }
 
     /* last item in the list is the destination path */
     const mfu_param_path* destpath = &paths[numpaths - 1];
@@ -256,22 +500,13 @@ int main(int argc, \
     int valid, copy_into_dir;
     mfu_param_path_check_copy(numpaths_src, paths, destpath, &valid, &copy_into_dir);
     mfu_copy_opts->copy_into_dir = copy_into_dir; 
-
     /* exit job if we found a problem */
-    if(! valid) {
+    if(!valid) {
         if(rank == 0) {
             MFU_LOG(MFU_LOG_ERR, "Exiting run");
         }
-        mfu_finalize();
-        MPI_Finalize();
-        return 1;
-    }
-
-    /* print usage if we need to */
-    if (usage) {
-        if (rank == 0) {
-            print_usage();
-        }
+        mfu_param_path_free_all(numpaths, paths);
+        mfu_free(&paths);
         mfu_finalize();
         MPI_Finalize();
         return 1;
@@ -279,23 +514,69 @@ int main(int argc, \
 
     /* create an empty file list */
     mfu_flist flist = mfu_flist_new();
+    if (run_as_daemon) {
+        while (1) {
+            if (rank == 0) {
+                memset(flist_name, 0, BUFFER_SIZE);
 
-    if (walk) {
-        /* walk paths and fill in file list */
-        int walk_stat = 1;
-        int dir_perm  = 0;
-        mfu_flist_walk_param_paths(numpaths_src, paths, walk_stat, dir_perm, flist);
+                if (!g_daemon) {
+                    g_daemon = MHD_start_daemon(MHD_USE_SELECT_INTERNALLY|MHD_USE_DEBUG,
+                                              PORT, NULL, NULL,&answer_to_connection,
+                                              (void *)greetingpage,
+                                              MHD_OPTION_NOTIFY_COMPLETED,
+                                              request_completed, NULL,
+                                              MHD_OPTION_END);
+                }
+
+                if (g_daemon == NULL) {
+                    snprintf(flist_name, 4, "NONE");
+                } else {
+                    int get_flist = 0;
+
+                    while (1) {
+                        if (strlen(flist_name) > 0)
+                            break;
+                        else
+                            sleep(1);
+                    }
+                }
+            }
+
+            MPI_Bcast(flist_name, BUFFER_SIZE, MPI_CHAR, 0, MPI_COMM_WORLD);
+            MPI_Barrier(MPI_COMM_WORLD);
+
+            if (!strncmp(flist_name, "NONE", 4))
+                break;
+
+            do_copy_with_flist(flist_name, flist, numpaths_src, paths,
+                               destpath, mfu_copy_opts);
+
+        }
     } else {
-        /* otherwise, read list of files from input, but then stat each one */
-        mfu_flist input_flist = mfu_flist_new();
-        mfu_flist_read_cache(inputname, input_flist);
-        mfu_flist_stat(input_flist, flist, mfu_input_flist_skip, NULL);
-        mfu_flist_free(&input_flist);
+        if (inputname == NULL) {
+            /* walk paths and fill in file list */
+            int walk_stat = 1;
+            int dir_perm  = 0;
+
+            mfu_flist_walk_param_paths(numpaths_src, paths, walk_stat, dir_perm, flist);
+        } else { 
+            struct mfu_flist_skip_args skip_args;
+
+            /* otherwise, read list of files from input, but then stat each one */
+            mfu_flist input_flist = mfu_flist_new();
+            MFU_LOG(MFU_LOG_INFO, "######:%s", inputname);
+            mfu_flist_read_cache(inputname, input_flist);
+
+            skip_args.numpaths = numpaths_src;
+            skip_args.paths = paths;
+            mfu_flist_stat(input_flist, flist, mfu_input_flist_skip, (void *)&skip_args);
+            mfu_flist_free(&input_flist);
+        }
+
+        /* copy flist into destination */ 
+        mfu_flist_copy(flist, numpaths_src, paths, destpath, mfu_copy_opts);
     }
 
-    /* copy flist into destination */ 
-    mfu_flist_copy(flist, numpaths_src, paths, destpath, mfu_copy_opts);
-    
     /* free the file list */
     mfu_flist_free(&flist);
 
